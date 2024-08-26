98aa8fd9c9a39efa50caf4bd8cc564da416eef91
91charu <134026214+91charu@users.noreply.github.com>
2024-08-25 21:33:15 -0500

    settingup question and quiz controller

---
+++ src/main/java/com/example/quizapp/QuestionController.java
@@ -0,0 +1,11 @@
+package com.example.quizapp;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+
+}
