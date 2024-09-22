110bfec0fa0e5721b319b63de81370bd175c912c
91charu <134026214+91charu@users.noreply.github.com>
2024-09-22 18:31:53 -0500

    working with dao's

--- src/main/java/com/example/quizapp/dao/QuestionDao.java
+++ src/main/java/com/example/quizapp/dao/QuestionDao.java
@@ -3,9 +3,11 @@ package com.example.quizapp.dao;
 import com.example.quizapp.model.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface QuestionDao extends JpaRepository<Question, Integer> {
 
     List<Question> findByCategory(String category);
--- src/main/java/com/example/quizapp/service/QuizService.java
+++ src/main/java/com/example/quizapp/service/QuizService.java
@@ -2,13 +2,19 @@ package com.example.quizapp.service;
 
 
 import com.example.quizapp.dao.QuestionDao;
+import com.example.quizapp.dao.QuizDao;
 import com.example.quizapp.model.Question;
+import com.example.quizapp.model.QuestionWrapper;
+import com.example.quizapp.model.Quiz;
+import com.example.quizapp.model.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class QuizService {
